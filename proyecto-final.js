5b19d642abb9396edeecf1acfed56e2a86c02df5
mlautaroj1 <123342007+mlautaroj1@users.noreply.github.com>
2023-02-23 14:24:39 -0300

    Add files via upload

---
+++ proyecto-final.js
@@ -0,0 +1,14 @@
+
+
+
+function mostrar() {
+  document.getElementById('datos').style.display = 'block';
+  document.getElementById('abrir').style.display = 'none';
+  document.getElementById('cerrar').style.display = 'block';
+}
+
+function ocultar() {
+  document.getElementById('datos').style.display = 'none';
+  document.getElementById('abrir').style.display = 'block';
+  document.getElementById('cerrar').style.display = 'none';
+}
